69abd0e0777a633a224105616eeeda7205ccd192
Zydiii <15826138027@163.com>
2018-12-21 00:14:29 +0800

    Mutiply/Divide

---
+++ Number Theory/divide.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct div{
+	int divide;
+	int rem;
+};
+
+bool is_even(int x) {
+	return x - x / 2 * 2;
+}
+
+struct div divide(int x, int y) {
+	struct div d;
+	if (x == 0) {
+		d.divide = 0;
+		d.rem = 0;
+		return d;
+	}
+	d = divide(x / 2, y);
+	d.divide = 2 * d.divide;
+	d.rem = 2 * d.rem;
+	if (is_even(x))
+		d.rem++;
+	if (d.rem >= y) {
+		d.rem -= y;
+		d.divide ++;
+	}
+	return d;
+}
+
+int main() {
+	int x, y;
+	cout << "x: ";
+	cin >> x;
+	cout << "y: ";
+	cin >> y;
+	struct div d = divide(x, y);
+	cout << d.divide << endl;
+	system("pause");
+}
---
+++ Number Theory/multiply.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool is_even(int x) {
+	return x - x / 2 * 2;
+}
+
+int multiply(int x, int y) {
+	if (!y)
+		return 0;
+	int z = multiply(x, y / 2);
+	if (!is_even(y))
+		return 2*z;
+	return 2*z + x;
+}
+
+int main() {
+	int x, y;
+	cout << "x: ";
+	cin >> x;
+	cout << "y: ";
+	cin >> y;
+	cout << "result:" << multiply(x, y) << endl;
+	system("pause");
+}
